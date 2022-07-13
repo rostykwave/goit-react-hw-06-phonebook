8bbe16986e89d30929e6c826b5af8237087e5347
Rostyslav Kovalchuk <rostykwave@GMAIL.COM>
2022-07-13 20:38:49 +0300

    removed AddContact

--- src/index.js
+++ src/index.js
@@ -4,9 +4,6 @@ import { App } from 'App';
 import './index.css';
 import { store } from 'redux/store';
 import { Provider } from 'react-redux';
-import { addContact } from 'redux/contacts/contacts-actions';
-
-// console.log(store.dispatch(addContact));
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
